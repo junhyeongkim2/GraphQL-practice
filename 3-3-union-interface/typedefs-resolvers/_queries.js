89ea349745ae675c199c8aa4d7bb211ed350a0f8
jun <ggprgrkjh@naver.com>
2022-10-16 01:59:17 +0900

    feat : union and interface

--- 3-3-union-interface/typedefs-resolvers/_queries.js
+++ 3-3-union-interface/typedefs-resolvers/_queries.js
@@ -1,13 +1,14 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-    type Query {
-        equipments: [Equipment]
-        equipmentAdvs: [EquipmentAdv]
-        softwares: [Software]
-        software: Software
-        supplies: [Supply]
-    }
-`
+  type Query {
+    equipments: [Equipment]
+    equipmentAdvs: [EquipmentAdv]
+    softwares: [Software]
+    software: Software
+    supplies: [Supply]
+    givens: [Given]
+  }
+`;
 
-module.exports = typeDefs
+module.exports = typeDefs;
